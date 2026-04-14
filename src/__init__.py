5f9d3386bfbd9bbdcc0c25b88a6afd7146b5a610
Al.Azeem <al.azeem002@gmail.com>
2026-04-14 11:28:50 +0100

    cleanup

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/__init__.py
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("task-automator")
-except PackageNotFoundError:
-    __version__ = "0.0.0-dev"
